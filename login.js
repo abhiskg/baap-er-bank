ac5a56ae4e4588ca506a25bbf38133cbc4c025f2
abhishek <abhiskg20@gmail.com>
2022-08-17 19:53:11 +0600

    Login Page complete

---
+++ login.js
@@ -0,0 +1,10 @@
+"use strict";
+// Submit button Handler
+const submitBtn = document.querySelector("#submit-btn");
+submitBtn.addEventListener("click", () => {
+    const email = document.querySelector("#email");
+    const password = document.querySelector("#password");
+    if (email.value === "abhi@gmail.com" && password.value === "123456") {
+        location.href = "bank.html";
+    }
+});
---
+++ src/login.ts
@@ -0,0 +1,9 @@
+// Submit button Handler
+const submitBtn = document.querySelector("#submit-btn") as HTMLButtonElement;
+submitBtn.addEventListener("click", () => {
+  const email = document.querySelector("#email") as HTMLInputElement;
+  const password = document.querySelector("#password") as HTMLInputElement;
+  if (email.value === "abhi@gmail.com" && password.value === "123456") {
+    location.href = "bank.html";
+  }
+});
