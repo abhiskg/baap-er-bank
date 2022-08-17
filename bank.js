7a291649cfa94da0a164e30247d0c37bf491f372
abhishek <abhiskg20@gmail.com>
2022-08-17 21:51:16 +0600

    Withdraw section complete

--- bank.js
+++ bank.js
@@ -1,19 +1,26 @@
 "use strict";
-const deposit = document.querySelector("#deposit");
-const withdraw = document.querySelector("#withdraw");
 const balance = document.querySelector("#balance");
-const preBalance = "1240";
-balance.textContent = preBalance;
 const depositBtn = document.querySelector("#deposit-btn");
 depositBtn.addEventListener("click", () => {
     const depositField = document.querySelector("#deposit-field");
+    const deposit = document.querySelector("#deposit");
     const depositAmount = parseFloat(deposit.textContent);
-    const newAmount = depositAmount + parseFloat(depositField.value);
-    deposit.textContent = newAmount.toString();
+    const newDepositAmount = depositAmount + parseFloat(depositField.value);
+    deposit.textContent = newDepositAmount.toString();
+    const balanceAmount = parseFloat(balance.textContent);
+    const newBalanceAmount = balanceAmount + parseFloat(depositField.value);
+    balance.textContent = newBalanceAmount.toString();
     depositField.value = "";
 });
 const withdrawBtn = document.querySelector("#withdraw-btn");
 withdrawBtn.addEventListener("click", () => {
+    const withdraw = document.querySelector("#withdraw");
     const withdrawField = document.querySelector("#withdraw-field");
-    withdraw.textContent = withdrawField.value;
+    const withdrawAmount = parseFloat(withdraw.textContent);
+    const newWithdrawAmount = withdrawAmount + parseFloat(withdrawField.value);
+    withdraw.textContent = newWithdrawAmount.toString();
+    const balanceAmount = parseFloat(balance.textContent);
+    const newBalanceAmount = balanceAmount - parseFloat(withdrawField.value);
+    balance.textContent = newBalanceAmount.toString();
+    withdrawField.value = "";
 });
--- src/bank.ts
+++ src/bank.ts
@@ -1,19 +1,20 @@
-const deposit = document.querySelector("#deposit") as HTMLSpanElement;
-const withdraw = document.querySelector("#withdraw") as HTMLSpanElement;
 const balance = document.querySelector("#balance") as HTMLSpanElement;
-
-const preBalance = "1240";
-
-balance.textContent = preBalance;
 
 const depositBtn = document.querySelector("#deposit-btn") as HTMLButtonElement;
 depositBtn.addEventListener("click", () => {
   const depositField = document.querySelector(
     "#deposit-field"
   ) as HTMLInputElement;
+  const deposit = document.querySelector("#deposit") as HTMLSpanElement;
+
   const depositAmount = parseFloat(deposit.textContent!);
-  const newAmount = depositAmount + parseFloat(depositField.value);
-  deposit.textContent = newAmount.toString();
+  const newDepositAmount = depositAmount + parseFloat(depositField.value);
+  deposit.textContent = newDepositAmount.toString();
+
+  const balanceAmount = parseFloat(balance.textContent!);
+  const newBalanceAmount = balanceAmount + parseFloat(depositField.value);
+  balance.textContent = newBalanceAmount.toString();
+
   depositField.value = "";
 });
 
@@ -21,8 +22,18 @@ const withdrawBtn = document.querySelector(
   "#withdraw-btn"
 ) as HTMLButtonElement;
 withdrawBtn.addEventListener("click", () => {
+  const withdraw = document.querySelector("#withdraw") as HTMLSpanElement;
   const withdrawField = document.querySelector(
     "#withdraw-field"
   ) as HTMLInputElement;
-  withdraw.textContent = withdrawField.value;
+
+  const withdrawAmount = parseFloat(withdraw.textContent!);
+  const newWithdrawAmount = withdrawAmount + parseFloat(withdrawField.value);
+  withdraw.textContent = newWithdrawAmount.toString();
+
+  const balanceAmount = parseFloat(balance.textContent!);
+  const newBalanceAmount = balanceAmount - parseFloat(withdrawField.value);
+  balance.textContent = newBalanceAmount.toString();
+
+  withdrawField.value = "";
 });
